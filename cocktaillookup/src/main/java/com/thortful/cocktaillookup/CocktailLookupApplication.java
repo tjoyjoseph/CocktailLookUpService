5666789ce4c86369819819c843e074e98ea2ccc3
tobyj <toby.workmail8@gmail.com>
2022-12-16 00:36:00 +0000

    api_endpoint: - added caching that expires in 1 day

--- cocktaillookup/src/main/java/com/thortful/cocktaillookup/CocktailLookupApplication.java
+++ cocktaillookup/src/main/java/com/thortful/cocktaillookup/CocktailLookupApplication.java
@@ -2,8 +2,10 @@ package com.thortful.cocktaillookup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
 
 @SpringBootApplication()
+@Cacheable
 public class CocktailLookupApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CocktailLookupApplication.class, args);
